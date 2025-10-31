5b6a06686d6b830b634f9d97142fbc3c8ca23549
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-10-31 17:08:33 +0200

    new1

---
+++ plan.js
@@ -0,0 +1,24 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDo = document.querySelector('.toDo');
+
+btn.addEventListener('click', function(){
+        
+    if (inputField.value.trim().length === 0) return;{
+
+    const item = document.createElement('li');
+    item.innerText =inputField.value();
+    item.classList.add('toDoAddet');
+    inputField.value = '';
+    toDo.appendChild(item);
+    
+    item.addEventListener('click', function(){
+        item.classList.add('newClass');
+    })
+
+    item.addEventListener('dblclick', function() {
+        toDo.removeChild(item);
+    })
+
+}
+})
